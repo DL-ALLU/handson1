9f8200e15b5ec34036528fc5d4345b3bd12ab417
DL-ALLU <dhivyalakshmialluece96@gmail.com>
2026-07-15 19:45:00 +0530

    created test3.py file

--- test2.py
+++ test2.py
@@ -5,6 +5,8 @@ browser.get("https://practicetestautomation.com/practice-test-login/")
 print("Open Browser")
 time.sleep(2)
 browser.maximize_window()
+title = browser.title
+print(title)
 time.sleep(2)
 browser.quit()
 time.sleep(2)
