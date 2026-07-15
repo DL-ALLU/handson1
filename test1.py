c8c79b672a52f83abd9324481d26c1e0fd53d1ec
DL-ALLU <dhivyalakshmialluece96@gmail.com>
2026-07-15 13:44:23 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,13 @@
-created a python file
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+browser = webdriver.Chrome()
+browser.get("https://practicetestautomation.com/practice-test-login/")
+print("Open Browser")
+time.sleep(2)
+browser.maximize_window()
+title = browser.title
+print(title)
+time.sleep(2)
+browser.quit()
